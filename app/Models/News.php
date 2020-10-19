44a3c397cf135f1aa95e829da39da518e2448d70
david <davidluengo8@gmail.com>
2020-10-19 17:35:18 +0200

    create seeds to insert fake data

--- app/Models/News.php
+++ app/Models/News.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class News extends Model
 {
+    /**
+     * In order to instantiate a factory instance for this model
+     * and create fake data
+     */
+    use HasFactory;
+
     /**
      * The attributes that are mass assignable.
      *
