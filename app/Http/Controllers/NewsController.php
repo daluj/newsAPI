18b0ca8aa7869b7158f386a3a9bd7956c5affc8b
david <davidluengo8@gmail.com>
2020-10-19 18:20:09 +0200

    Implement get home news #2

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+
 class NewsController extends Controller
 {
-    //
+    /**
+     * Function that returns all news on database
+     */
+    public function showAllNews()
+    {
+        // Build response
+        $response = array(
+            'data' => News::all(),
+            'message' => 'OK'
+        );
+
+        // Return the response in JSON format
+        return response()->json($response);
+    }
 }
